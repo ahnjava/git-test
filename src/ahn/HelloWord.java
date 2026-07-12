d2f7735bce8a4de3308de8bc903853b801aa115b
DESKTOP-P3ML9QV\ahnja <ahnjava@naver.com>
2026-07-12 09:35:02 +0900

    프로젝트 B 소스 추가 -- B

--- src/ahn/HelloWord.java
+++ src/ahn/HelloWord.java
@@ -5,6 +5,8 @@ public class HelloWord {
 	public static void main(String[] args) {
 		
 		System.out.println("==== [Project A] ====");
+		
+		System.out.println("==== [Project B] 2. B 소스 추가 ====");
 
 	}
 
